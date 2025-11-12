9431c402ba05ba368f3d22605ef699b84a06d7d7
Tomaž Pristovnik <tomaz.pristovnik@gmail.com>
2025-11-12 10:49:02 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Script from "next/script";
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
+    <header className="pt-20 pb-12 relative">
+      {/* Load D-ID Agent Script */}
+      <Script
+        type="module"
+        src="https://agent.d-id.com/v2/index.js"
+        data-mode="fabio"
+        data-client-key="Z29vZ2xlLW9hdXRoMnwxMTM2MzE3ODYyMzAxMjkwNDc3NTk6SWFFOVpiNmw2Yk5pOWRLaW5JTmdQ"
+        data-agent-id="v2_agt_br-R4l6H"
+        data-name="did-agent"
+        data-monitor="true"
+        data-orientation="horizontal"
+        data-position="right"
+        strategy="afterInteractive" // ensures it loads only in browser
+      />
+
       <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
